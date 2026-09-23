a39964a9288915f65d112d3cab01366d59024bc6
RikuSpl <kiituriii@proton.me>
2026-09-23 15:38:36 +0300

    added copying functionality and hover/click effects

--- src/ColorItem.jsx
+++ src/ColorItem.jsx
@@ -1,10 +1,44 @@
+import { useState } from "react"
 import "./index.css"
 
 function ColorItem({ color, hex }) {
 
+    const [hoverState, setHoverState] = useState(false)
+    const [copied, setCopied] = useState(hex)
+
+    function handleMouseEnter() {
+        setHoverState(true)
+    }
+
+    function handleMouseLeave() {
+        setHoverState(false)
+        setCopied(hex)
+    }
+
+    function handleClick() {
+        navigator.clipboard.writeText(hex)
+        setCopied("Copied!")
+    }
+
     return (
         <>
-            <li className={`color-item ${color}`} style={{backgroundColor: hex}}></li>
+            {hoverState === true ? 
+                (<li 
+                className={`color-item`} 
+                style={{backgroundColor: hex}}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onClick={handleClick}
+                ><span>{copied}</span></li>) : (
+                    <li 
+                    className={`color-item`} 
+                    style={{backgroundColor: hex}}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    ><span>{color}</span></li>
+                )
+            }
+            
         </>
     )
 }
